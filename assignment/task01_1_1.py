cfd377a5529c6f7baf686814e69218e5039e96c3
王顶 <408542507@qq.com>
2016-09-22 06:58:03 +0800

    金子塔的最简单实现方式。

---
+++ assignment/task01_1_1.py
@@ -0,0 +1,12 @@
+#! /user/bin/env python
+# _*_  coding: utf-8  _*_
+# __author__ = "王顶"
+# Email: 408542507@qq.com
+
+"""
+最简单的实现方式，不考虑的可扩展性
+"""
+print("   *")
+print("  ***")
+print(" *****")
+print("*******")
